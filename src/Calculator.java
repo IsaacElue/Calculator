b129efc5716f9218e499b8189dd5e37b662f11e9
IsaacElue <elueisaac14@gmail.com>
2024-07-19 17:22:34 +0100

    Final Edits

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,35 +1,40 @@
+//all libraries required
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class App implements ActionListener {
+//this is my first java project utilising the swing library
+//this project creates a simple calculator 
+//*changes* - edited variables 
+
+public class Calculator implements ActionListener {
     JFrame frame;
     JTextField textField;
-    JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[9];
+    JButton[] numbers = new JButton[10];
+    JButton[] functions = new JButton[9];
     JButton addButton, subButton, mulButton, divButton;
     JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
-    Font myFont = new Font("Times New Roman", Font.PLAIN, 30);
-    double num1 = 0, num2 = 0, result = 0;
-    char operator;
+    Font font = new Font("Times New Roman", Font.PLAIN, 30);
+    double num1 = 0, num2 = 0, result = 0; //starting numbers 
+    char operators; // signs
 
-    App()
+    Calculator()
     {
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setBackground(Color.black);
         frame.setLayout(null);
-        //i have now created the basic frame for a calculator with a title 
+        //I have now created the basic frame for a calculator with a title 
 
-        //now i work on the textfield
+        //working on the textfield
         textField = new JTextField();
         textField.setBounds(50, 25, 300, 50);
-        textField.setFont(myFont);
+        textField.setFont(font);
         textField.setEditable(false);
 
-        //adding the functions
+        //adding visual functionality to each button and function 
         addButton = new JButton("+");
         subButton = new JButton("-");
         mulButton = new JButton("x");
@@ -40,31 +45,32 @@ public class App implements ActionListener {
         clrButton = new JButton("clr");
         negButton = new JButton("(-)");
 
-        functionButtons[0] = addButton;
-        functionButtons[1] = subButton;
-        functionButtons[2] = mulButton;
-        functionButtons[3] = divButton;
-        functionButtons[4] = decButton;
-        functionButtons[5] = equButton;
-        functionButtons[6] = delButton;
-        functionButtons[7] = clrButton;
-        functionButtons[8] = negButton;
+        functions[0] = addButton;
+        functions[1] = subButton;
+        functions[2] = mulButton;
+        functions[3] = divButton;
+        functions[4] = decButton;
+        functions[5] = equButton;
+        functions[6] = delButton;
+        functions[7] = clrButton;
+        functions[8] = negButton;
 
         for(int i = 0; i <9; i++)
         {
-            functionButtons[i].addActionListener(this);
-            functionButtons[i].setFont(myFont);
-            functionButtons[i].setFocusable(false);
+            functions[i].addActionListener(this);
+            functions[i].setFont(font);
+            functions[i].setFocusable(false);
         }
 
         for(int i = 0; i <10; i++)
         {
-            numberButtons[i] = new JButton(String.valueOf(i));
-            numberButtons[i].addActionListener(this);
-            numberButtons[i].setFont(myFont);
-            numberButtons[i].setFocusable(false);
+            numbers[i] = new JButton(String.valueOf(i));
+            numbers[i].addActionListener(this);
+            numbers[i].setFont(font);
+            numbers[i].setFocusable(false);
         }
 
+        //positioning of the 3 buttons at the bottom of the calculator
         negButton.setBounds(50, 430, 100, 50);
         delButton.setBounds(150, 430, 100, 50);
         clrButton.setBounds(250, 430, 100, 50);
@@ -74,29 +80,22 @@ public class App implements ActionListener {
         panel.setBounds(50, 100, 300, 320);
         panel.setLayout(new GridLayout(4,4,10,10));
 
-        
-
-
-        //adding numbers to the panel
-        panel.add(numberButtons[1]);
-        panel.add(numberButtons[2]);
-        panel.add(numberButtons[3]);
+        //adding numbers to the panel in standard calculator 
+        panel.add(numbers[1]);
+        panel.add(numbers[2]);
+        panel.add(numbers[3]);
         panel.add(addButton);
-
-        panel.add(numberButtons[4]);
-        panel.add(numberButtons[5]);
-        panel.add(numberButtons[6]);
+        panel.add(numbers[4]);
+        panel.add(numbers[5]);
+        panel.add(numbers[6]);
         panel.add(subButton);
-
-        panel.add(numberButtons[7]);
-        panel.add(numberButtons[8]);
-        panel.add(numberButtons[9]);
+        panel.add(numbers[7]);
+        panel.add(numbers[8]);
+        panel.add(numbers[9]);
         panel.add(mulButton);
-
         panel.add(decButton);
-        panel.add(numberButtons[0]);
+        panel.add(numbers[0]);
         panel.add(equButton);
-
         panel.add(divButton);
        
         //adding all panels and extra buttons to the frame
@@ -109,21 +108,22 @@ public class App implements ActionListener {
     }
     public static void main(String[] args) throws Exception 
     { 
-        App calculator = new App();
+        Calculator x = new Calculator();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) 
     {
-
         for(int i = 0; i < 10; i++)
         {
-            if(e.getSource()== numberButtons[i])
+            if(e.getSource()== numbers[i])
             {
                 textField.setText(textField.getText().concat(String.valueOf(i)));
             }
             //this allows the numbers to be pressed as a function button and displayed on the text field as an action 
         }
+
+
 
         if(e.getSource()==decButton)
         {
@@ -134,7 +134,7 @@ public class App implements ActionListener {
         if(e.getSource()==equButton)
         {
             num2 = Double.parseDouble(textField.getText());
-            switch(operator)
+            switch(operators)
             {
                 case'+':
                 result = num1+num2;
@@ -164,48 +164,50 @@ public class App implements ActionListener {
         }
         if(e.getSource()==delButton)
         {
-            String string = textField.getText();
+            String x = textField.getText();
             textField.setText("");
-            for(int i = 0; i < string.length()-1; i++)
+            for(int i = 0; i < x.length()-1; i++)
             {
-                textField.setText(textField.getText()+string.charAt(i));
+                textField.setText(textField.getText()+x.charAt(i));
             }
             //this function deletes the numbers from the last number one by one 
         }
 
         if(e.getSource()==negButton)
         {
-            double temp = Double.parseDouble(textField.getText());
-            temp*=-1;
-            textField.setText(String.valueOf(temp));
+            double y = Double.parseDouble(textField.getText());
+            y*=-1;
+            textField.setText(String.valueOf(y));
             //allowing a negative button to be used before a number 
         }
 
+
+        //operators
         if(e.getSource()==addButton)
         {
             num1 = Double.parseDouble(textField.getText());
-            operator = '+';
+            operators = '+';
             textField.setText("");
             //addition 
         }
         if(e.getSource()==subButton)
         {
             num1 = Double.parseDouble(textField.getText());
-            operator = '-';
+            operators = '-';
             textField.setText("");
             //subtraction 
         }
         if(e.getSource()==mulButton)
         {
             num1 = Double.parseDouble(textField.getText());
-            operator = 'x';
+            operators = 'x';
             textField.setText("");
             //multiplication 
         }
         if(e.getSource()==divButton)
         {
             num1 = Double.parseDouble(textField.getText());
-            operator = '/';
+            operators = '/';
             textField.setText("");
             //division
         }
